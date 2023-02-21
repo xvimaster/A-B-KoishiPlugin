d8682ddc99f65c9b006d24a98c3ebe0b2e3a3aa4
xvimaster <58104942+xvimaster@users.noreply.github.com>
2023-02-21 11:02:42 +0800

    Add files via upload
    bugfix,

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Context, Schema } from 'koishi'
+import { REPL_MODE_SLOPPY } from 'repl'
 
 export const name = 'a2b'
 
@@ -20,7 +21,7 @@ export function apply(ctx: Context) {
         if(txtProceed.charAt(i)==" ") commandCount++
       }
       console.info("---------------------------------------")
-      console.info(commandCount)
+
       var commands=txtProceed.split(" ",commandCount+1)
 
       //A=B language part
@@ -30,32 +31,109 @@ export function apply(ctx: Context) {
       var nowCommand=" " //which command is running now
 
       //A=B special commands variables
-
+      var isReturn=0 //is there an return command in the second section of the command
+      var isOnce=0 //is there an Once command in the first section of the command
+      var replaceMark=0
+      //is there start or end command on L or R side
+      var isLStart=0
+      var isRStart=0
+      var isLEnd=0
+      var isREnd=0 
       
-      
-      while(process<=commandCount && antiDeadLoop<1000){
-        // now not available with "return""start""end""once"commands
-        nowCommand=commands[process]
-        var ios=nowCommand.split("=",2)
-        var replaceMark=A2BString.indexOf(ios[0]) 
-
-        console.info(nowCommand)
-        console.info(replaceMark)
-
-        if(replaceMark>=0){  //if this command can run
-          var piece1=A2BString.substring(0,replaceMark);
-          var piece2=A2BString.substring(replaceMark+ios[0].length,A2BString.length)// i hope this wont cause any bug
-          A2BString=piece1.concat(ios[1],piece2)
-          process=1;
-          console.info(A2BString+" this turn")
-          antiDeadLoop++;
-        }else{
-          process++; //move to next command
-        }
+    
+      while(process<=commandCount && antiDeadLoop<100){
         
+        nowCommand=commands[process]
+        if(nowCommand!="NULL"){
+
+          console.info("A2BString: " + A2BString)
+
+          var ios=nowCommand.split("=",2)
+          isOnce=ios[0].indexOf("(once)")
+          isReturn=ios[1].indexOf("(return)")
+          
+          isLStart=ios[0].indexOf("(start)")
+          isLEnd=ios[0].indexOf("(end)")
+          isRStart=ios[1].indexOf("(start)")
+          isREnd=ios[1].indexOf("(end)")
+          var EndIndicator //calculates where the end mark Should be
+
+          if((isLStart!=-1 && isLEnd!=-1)||(isLStart!=-1 && isOnce!=-1)||(isOnce!=-1 && isLEnd!=-1)) return "不允许在左侧有多个关键字！"
+          if((isRStart!=-1 && isREnd!=-1)||(isRStart!=-1 && isReturn!=-1)||(isReturn!=-1 && isREnd!=-1)) return "不允许在右侧有多个关键字！"
+
+
+          if(isLStart!=-1){
+            ios[0]=ios[0].substring(7,ios[0].length)
+            if(A2BString.indexOf(ios[0])==0)replaceMark=0; else replaceMark=-1;
+            //only matches the start section of the string
+            if(A2BString.indexOf(ios[0])==0) A2BString=A2BString.substring(ios[0].length,A2BString.length)
+            //remove the start section in A2BString
+          }else if(isLEnd!=-1){
+            ios[0]=ios[0].substring(5,ios[0].length)
+            EndIndicator=A2BString.length-ios[0].length 
+            if(A2BString.lastIndexOf(ios[0])==EndIndicator)replaceMark=EndIndicator; else replaceMark=-1;
+            //only matches the end section of the string
+            if(A2BString.lastIndexOf(ios[0])==EndIndicator) A2BString=A2BString.substring(0,A2BString.length-ios[0].length)
+            //remove the end section in A2BString
+            console.info("LEndIndicator:"+EndIndicator)
+
+          }else if(isOnce!=-1){
+            ios[0]=ios[0].substring(6,ios[0].length)
+            replaceMark=A2BString.indexOf(ios[0])
+            A2BString=A2BString.substring(0,replaceMark)+A2BString.substring(replaceMark+ios[0].length,A2BString.length)
+            //now neutrialize the command
+            commands[process]="NULL"
+          }else{ //not once, not start and not end
+            replaceMark=A2BString.indexOf(ios[0]) //look for the command string in A2BString
+            if(replaceMark!=-1) A2BString=A2BString.substring(0,replaceMark)+A2BString.substring(replaceMark+ios[0].length,A2BString.length)
+          }
+            
+          
+          //---DEBUG---
+          console.info(nowCommand)
+          console.info(replaceMark)
+          console.info("---")
+          //---DEBUG---*/
+
+          if(replaceMark>=0){  //if this command can run
+            
+            if(isReturn!=-1){ //check for straight return
+              A2BString=ios[1].substring(8,ios[1].length)
+              return A2BString
+            }else if(isRStart!=-1){
+              ios[1]=ios[1].substring(7,ios[1].length)
+              A2BString=ios[1]+A2BString
+              antiDeadLoop++
+              process=1;
+            }else if(isREnd!=-1){
+              ios[1]=ios[1].substring(5,ios[1].length)
+              A2BString=A2BString+ios[1]
+              antiDeadLoop++
+              process=1;
+            }else if(isLStart==1){
+              A2BString=ios[1]+A2BString
+              antiDeadLoop++
+              process=1;
+            }else if(isLEnd==1){
+              A2BString=A2BString+ios[1]
+              antiDeadLoop++
+              process=1;
+            }else{
+
+              A2BString=A2BString.substring(0,replaceMark)+ios[1]+A2BString.substring(replaceMark,A2BString.length)
+              process=1;
+              antiDeadLoop++;
+            }
+
+          }else{
+            process++; //move to next command
+          }
+        }else{
+          process++;  //move to the next command while the command is neutrialized
+        }
       }
       if (A2BString.length==0) return "程序无输出"
-      if (A2BString.length>255) return "输出大于255个字符。请检查潜在的死循环"
+      if (A2BString.length>255) return "输出大于255个字符或处理大于1000步。请检查潜在的死循环"
       return A2BString
 
     } else {
